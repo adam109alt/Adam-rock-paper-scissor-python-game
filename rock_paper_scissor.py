6c02f2c09f38b09823836c17767ab11193c28bf5
adam109alt <eagletube10@gmail.com>
2026-01-14 17:35:17 +0300

    Refactor game logic and improve user input handling
    Refactor rock paper scissors game logic into functions and improve input handling.

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,58 +1,27 @@
+# rock paper scissor v3
+
 import random
 
+
 while True:
+    def play():
+        user = input('Choose one Rock, Paper, Scissor: ').lower()
+        computer = random.choice(['rock','paper','scissors'])
+        print(f'The computer choose {computer}, And you choose: {user}')
+
+        if user == computer:
+            return 'Tie!'
+        
+        if is_win(user , computer):
+            return 'You won'
+        
+        return 'You lost!'
+            
+    def is_win(player , enemy):
+        if player == 'rock' and enemy == 'scissors' or player == 'paper' and enemy == 'rock' or player == 'scissors' and enemy == 'paper':
+            return True
+
+    print(play())
     
-    gusse = input('Enter your gusse: ').lower()
-    
-    if gusse == 'rock':
-        print(f'You choose {gusse}')
-    elif gusse == 'scssior':
-        print(f'You choose {gusse}')
-    elif gusse == 'paper':
-        print(f'you choose {gusse}')
-        
-    else:
-        print('Invalid')
-        
-    cont = input('If you want to continue type y and if you want to stop type n: ')
-    
-    if cont == 'y':
-        computer_gusses = random.randint(1 , 3)
-        if computer_gusses == 1:
-            print(f'The computer gusse is: Rock')
-        elif computer_gusses == 2:
-            print(f'The computer gusse is: Scissor')
-        elif computer_gusses == 3:
-            print(f'The computer gusse is: Paper')
-        
-    elif cont == 'n':
-        print('BYE!')
-        break
-    else:
-        print('Invalid!')
-        
-    if computer_gusses == 1 and gusse == 'rock':
-        print('Its tie ')
-        
-    elif computer_gusses == 2 and gusse == 'scssior':
-        print('Its tie ')
-        
-    elif computer_gusses == 3 and gusse == 'paper':
-        print('Its tie ')
-        
-    elif computer_gusses == 1 and gusse == 'scssior':
-        print('Computer won')
-        
-    elif computer_gusses == 2 and gusse == 'rock':
-        print('You win')
-        
-    elif computer_gusses == 3 and gusse == 'scssior':
-        print('you win')
-        
-    elif computer_gusses == 2 and gusse == 'paper':
-        print('compuer win')
-        
-    elif computer_gusses == 3 and gusse == 'rock':
-        print('you win')
-    elif computer_gusses == 1 and gusse == 'paper':
-        print('you win')
+
+
